e4ab2f1b1cc547a924804c04a4b831e089c26ee8
samskivert <samskivert@f48a3320-ce8d-11de-bb02-e302c5db49d5>
2009-11-12 02:38:28 +0000

    Proper handling for "this" qualified names when inferencing. Come to think of it, I probably don't handle "ClassName.this" qualified names, but a man can only do so much. This might result in a falsely assumed final which will break your compile and which you can then easily fix.
    Also added heuristic to not note mutations of object fields done in a
    constructor. Similarly this may false-positive tag something as immutable which
    is reassigned in the constructor and only in the constructor. Probably not
    common and easy to fix when the compiler fails your build.

--- src/org/immutablej/imferrer/ImferContext.java
+++ src/org/immutablej/imferrer/ImferContext.java
@@ -9,6 +9,7 @@ import java.util.Map;
 import java.util.Set;
 
 import com.sun.tools.javac.code.Scope;
+import com.sun.tools.javac.code.Symbol.ClassSymbol;
 import com.sun.tools.javac.code.Symbol;
 import com.sun.tools.javac.tree.JCTree.JCVariableDecl;
 
@@ -26,6 +27,9 @@ public class ImferContext
     /** The set of all symbols for which mutability was inferred. */
     public Set<Symbol> mutable = new HashSet<Symbol>();
 
+    /** The symbol of the class whose constructor body we're processing, or null. */
+    public ClassSymbol cting;
+
     /**
      * Duplicates this context with the specified new scope.
      */
@@ -35,6 +39,7 @@ public class ImferContext
         ctx.scope = scope;
         ctx.symToDecl = symToDecl;
         ctx.mutable = mutable;
+        ctx.cting = cting;
         return ctx;
     }
 }
